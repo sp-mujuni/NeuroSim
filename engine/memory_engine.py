a06db77f715eb11b139f8741102c752800eec113
sp-mujuni <smujuni99@gmail.com>
2025-07-04 00:34:03 +0300

    Add get_latest_book_balance method to MemoryEngine for retrieving book balance

--- engine/memory_engine.py
+++ engine/memory_engine.py
@@ -1,6 +1,6 @@
 import time
 import uuid
-from db.orm import save_memory_entry, load_all_memories, update_memory_entry, get_memory_by_id
+from db.orm import save_memory_entry, load_all_memories, update_memory_entry, get_memory_by_id, get_latest_book_balance
 
 class MemoryEngine:
     def __init__(self):
@@ -12,12 +12,19 @@ class MemoryEngine:
 
     def get_memories(self):
         return self.memory_store
+    
+    def get_latest_book_balance(self, book_id):
+        for mem in self.memory_store:
+            if mem["book_id"] == book_id:
+                return get_latest_book_balance(book_id)
 
     def get_memory_by_id(self, memory_id):
         for mem in self.memory_store:
             if mem["id"] == memory_id:
                 return mem
         return None
+    
+
 
     def update_memory(self, updated_mem):
         for i, mem in enumerate(self.memory_store):
