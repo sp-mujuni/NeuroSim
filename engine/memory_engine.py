0693c1961dcaf6b02b7123dcea8e45de768a4eac
sp-mujuni <smujuni99@gmail.com>
2025-07-03 13:17:40 +0300

    Refactor MemoryEngine to load memories from the database and remove unused methods

--- engine/memory_engine.py
+++ engine/memory_engine.py
@@ -1,29 +1,27 @@
 import time
 import uuid
-from db.orm import save_memory_entry
+from db.orm import save_memory_entry, load_all_memories, update_memory_entry, get_memory_by_id
 
 class MemoryEngine:
     def __init__(self):
-        self.memory_store = []  # temporary in-memory list; production should use persistent DB
+        self.memory_store = load_all_memories()
 
-    def encode_observations(self, observations):
-        for obs in observations:
-            memory = {
-                "id": str(uuid.uuid4()),
-                "timestamp": time.time(),
-                "content": obs.get("resource", {}).get("valueQuantity", {}).get("value", "Unknown"),
-                "type": obs.get("resource", {}).get("code", {}).get("text", "Observation"),
-                "strength": 1.0,
-                "context_tags": [obs.get("resource", {}).get("code", {}).get("coding", [{}])[0].get("code", "unknown")]
-            }
-            self.memory_store.append(memory)
-            save_memory_entry(memory)
+    def save_memory(self, memory):
+        self.memory_store.append(memory)
+        save_memory_entry(memory)
 
     def get_memories(self):
         return self.memory_store
 
-    def update_strength(self, memory_id, new_strength):
+    def get_memory_by_id(self, memory_id):
         for mem in self.memory_store:
             if mem["id"] == memory_id:
-                mem["strength"] = new_strength
+                return mem
+        return None
+
+    def update_memory(self, updated_mem):
+        for i, mem in enumerate(self.memory_store):
+            if mem["id"] == updated_mem["id"]:
+                self.memory_store[i] = updated_mem
+                update_memory_entry(updated_mem)
                 break
